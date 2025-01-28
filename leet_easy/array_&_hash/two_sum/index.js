2e03c0bb0d9595a1a7c8ab8b28d10b69bbcfa9a2
VladeanClaudiu <claudiu@danusports.com>
2025-01-28 15:55:26 +0000

    changes to how the search is done

--- leet_easy/array_&_hash/two_sum/index.js
+++ leet_easy/array_&_hash/two_sum/index.js
@@ -1,16 +1,29 @@
 
 
 const twoSum = (nums, target) => {
-    let firstIndex = 0
-    let secondIndex = 0
+    // let firstIndex = 0
+    // let secondIndex = 0
+
+
+    // for(let i = 0; i < nums.length; i++) {
+    //     firstIndex++;
+    //     const diff =  target-nums[i]
+    //      if(nums.slice(firstIndex).includes(diff)){
+    //         secondIndex = nums.slice(firstIndex).indexOf(diff)+firstIndex
+    //         return[i, secondIndex]
+    //     }
+    // }
+
+    const prevMap = new Map()
 
 
     for(let i = 0; i < nums.length; i++) {
-        firstIndex++;
-         if(nums.slice(firstIndex).includes(target-nums[i])){
-            secondIndex = nums.slice(firstIndex).indexOf(target-nums[i])+firstIndex
-            return[i, secondIndex]
-        }
-    }
+        const diff = target - nums[i]
 
+        if(prevMap.has(diff)){
+            return [prevMap.get(diff), i]
+        }
+
+        prevMap.set(nums[i], i)
+    }
 }
